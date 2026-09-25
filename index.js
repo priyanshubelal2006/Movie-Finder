573aa1e8135046350d91f2fc43dee3e618ee93d0
priyanshu belal <singhpriyanshu7131@gmail.com>
2026-09-25 20:58:33 +0530

    First Commimt

---
+++ index.js
@@ -0,0 +1,50 @@
+let form = document.querySelector(".frm");
+let input = document.querySelector(".frm-inpt");
+let btn = document.querySelector(".btn")
+let validMovie = document.querySelector(".validMovie")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let query = input.value.trim();
+
+    if (!query) {
+        return;
+    }
+    searchMovie(query);
+})
+
+async function searchMovie(movieName) {
+    validMovie.innerHTML = `<div class="loader"></div>`
+    let response = await fetch(`http://www.omdbapi.com/?apikey=6ed6e6bf&s=${encodeURIComponent(movieName)}`)
+    let data = await response.json();
+    console.log(data);
+    if (data.Response === "True") {
+        displayMovies(data.Search)
+    }
+    else {
+        validMovie.innerHTML = `<p>${data.Error}</p>`
+    }
+}
+searchMovie("spider")
+
+function displayMovies(movies) {
+    validMovie.innerHTML = "";
+    movies.forEach(movie => {
+        const div = document.createElement("div");
+        div.dataset.id = movie.imdbID;
+        div.classList.add("movie-card");
+        div.innerHTML = `<img src="${movie.Poster}">
+   <h1>title: ${movie.Title} </h1>
+   <h1>Year: ${movie.Year}</h1>
+   `
+        validMovie.append(div);
+    });
+}
+
+validMovie.addEventListener('click', e => {
+    e.stopPropagation();
+    const movieCard = e.target.closest(".movie-card");
+    const imdbID = movieCard.dataset.id
+    location.href = `movie-details.html?id=${imdbID}`
+})
+
---
+++ movie-details.js
@@ -0,0 +1,57 @@
+const movieDetail = document.querySelector(".movie-detail")
+const params = new URLSearchParams(location.search);
+const imdbId = params.get("id");
+
+if (imdbId) {
+    searchMovie(imdbId.trim());
+}
+
+async function searchMovie(movieName) {
+    movieDetail.innerHTML = `<div class="loader"></div>`
+    let response = await fetch(`http://www.omdbapi.com/?apikey=6ed6e6bf&i=${imdbId}`)
+    let data = await response.json();
+    console.log(data);
+    if (data.Response === "True") {
+        displayMovie(data)
+    }
+    else {
+        console.log(data.error);
+    }
+}
+
+function displayMovie(data) {
+
+    movieDetail.innerHTML =
+        `  <div>
+            <img src=${data.Poster} alt="">
+        </div>
+        <div>
+            <h2>${data.Title}</h2>
+            <section>
+                <p>${data.Released}</p>
+                <p>${data.Rated}</p>
+                <p>${data.Runtime}</p>
+                <p>${data.Genre}</p>
+                <p>${data.imdbRating}/10</p>
+            </section>
+        </div>
+        <div>
+            <p>Plot Overview</p>
+            <p>${data.Plot}</p>
+        </div>
+        <div>
+            <p>Director</p>
+            <p>${data.Director}</p>
+        </div>
+        <div>
+            <p>Writer</p>
+            <p>${data.Writer}</p>
+        </div>
+        <div>
+            <p>Actors</p>
+            <p>${data.Actors}</p>
+        </div>
+        <a href=https://www.imdb.com/title/${data.imdbID} target=_blank> <button>Watch Movie </button></a>`
+
+}
+
